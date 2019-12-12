0a06cdfadccd6548023e637697e1d959f960d7c0
Mallick <jmallick@MALLICKMBP.local>
2019-12-12 20:58:59 +0530

    updating the master

---
+++ masterrepo/shellscripts/for.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#traditional Language
+#for (int i=0 ; i <=10;i++){
+ #system.out.println("Hello World");
+#}
+
+for var1 in 1 2 3
+do
+   for var2 in 0 5
+   do
+      if [ $var1 -eq 2 -a $var2 -eq 0 ]
+      then
+         break 2
+      else
+         echo "$var1 $var2"
+      fi
+   done
+done
