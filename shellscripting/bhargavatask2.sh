f53f0a68e5a1de24f9921f67c28f73a48ceaa1c3
bhargav visu <bhargava.viswanath@gmail.com>
2019-12-12 12:35:05 -0500

    text equal task

--- shellscripting/bhargavatask2.sh
+++ shellscripting/bhargavatask2.sh
@@ -4,8 +4,16 @@ a=lucifer
 b=lucifer
 c=bhargava
 
-if [ $a = $b ]
+if [ $a == $b ]
 then
 echo "text is same "
- 
+else 
+echo " text is not same"
+fi
+if [ $a == $c ]
+then 
+echo " text is same "
+else 
+echo "text is not same"
+
 fi
