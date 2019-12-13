c04f8d8b3b58af3b69867483101accf3f14583cd
bhargav visu <bhargava.viswanath@gmail.com>
2019-12-13 02:02:22 -0500

    ifcase

---
+++ bhargav/bhargavatask2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+a=lucifer
+b=lucifer
+c=bhargava
+
+if [ $a == $b ]
+then
+echo "text is same "
+else 
+echo " text is not same"
+fi
+if [ $a == $c ]
+then 
+echo " text is same "
+else 
+echo "text is not same"
+
+fi
---
+++ bhargav/bhargavatask3.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+read n
+echo "value is $n "
+for (( c=1; c<=5; c++ ))
+do  
+   echo " I am good "
+done
