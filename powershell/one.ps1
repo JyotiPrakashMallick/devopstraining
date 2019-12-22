1805bf6e97eb0ead03b6a3999b5f1f4f5be6841e
lyceeott <57507039+lyceeott@users.noreply.github.com>
2019-12-22 11:42:01 -0500

    Update one.ps1
    changed elseif to else.

--- powershell/one.ps1
+++ powershell/one.ps1
@@ -7,7 +7,7 @@ $c = Read-Host -promt 'input your Third number'
  if($a -gt $b -and $a -gt $c){
      Write-Host "First number is greater"
  } elseif($b -gt $a -and $b -gt $c ){
-     Write-Host "second bumber is greater"
- } elseif($c -gt $b -and $c -gt $a){
+     Write-Host "second number is greater"
+ } else($c -gt $b -and $c -gt $a){
      Write-Host ("third number is greater")
  }
